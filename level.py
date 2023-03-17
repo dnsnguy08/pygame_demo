a3b6c0341b306ea103d1c863e737b8c8e2694a86
Dennis Nguyen <dnsnguy08@gmail.com>
2023-03-17 14:08:16 -0700

    render player and obstacles on map

--- level.py
+++ level.py
@@ -1,5 +1,8 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
 
 
 class Level:
@@ -9,15 +12,23 @@ class Level:
 
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
 
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
-            print(row_index, row)
+            for col_index, column in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if column == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if column == 'p':
+                    self.player = Player((x, y), [self.visible_sprites])
 
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
